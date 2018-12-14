c5002b2624d846bbe4446f262261316514c95aa6
cryptapus <info@cryptap.us>
2018-12-14 07:48:52 -0600

    gitian: more name changes

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -146,7 +146,7 @@ void setupAmountWidget(QLineEdit *widget, QWidget *parent)
 bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 {
     // return if URI is not valid or is no bitcoin: URI
-    if(!uri.isValid() || uri.scheme() != QString("bitcoin"))
+    if(!uri.isValid() || uri.scheme() != QString("riecoin"))
         return false;
 
     SendCoinsRecipient rv;
